9088885ec96443923d50d7462560483c19e8da0b
Sam Anthony <sam@samanthony.xyz>
2023-12-04 17:06:20 -0500

    use stack for part 2

--- 2023/04/p2.c
+++ 2023/04/p2.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "header.h"
+#include "stack.h"
 
 #define MAXCARDS 256
 
@@ -10,9 +11,9 @@ main()
 {
 	FILE *file;
 	char line[MAXLINE];
-	int winning[MAXWINNING], have[MAXHAVE];
-	int nwinning, nhave;
-	int i, num, card, cards;
+	int winning[MAXWINNING], nwinning;
+	Stack *have;
+	int i, num, card, cards, nhavewinning;
 	int copies[MAXCARDS];
 
 	for (i = 0; i < MAXCARDS; i++)
@@ -22,14 +23,14 @@ main()
 		printf("failed to open %s\n", FNAME);
 		return 1;
 	}
-
+	have = NULL;
 	cards = 0;
 	for (card = 1; fgets(line, MAXLINE, file) != NULL; card++) {
 		/* skip `Card %d:` */
 		for (i = 6; line[i] != ':'; i++)
 			;
 
-		nwinning = nhave = num = 0;
+		nwinning = num = 0;
 		/* get winning numbers */
 		while (line[++i] != '|') {
 			if (isdigit(line[i])) {
@@ -45,32 +46,29 @@ main()
 			}
 		}
 		/* get numbers we have */
+		stfree(have);
 		while (line[++i] != '\0') {
 			if (isdigit(line[i])) {
 				num = (num * 10) + (line[i] - '0');
-			} else if (nhave < MAXHAVE) {
-				if (num > 0)
-					have[nhave++] = num;
+			} else if (num > 0) {
+				have = stpush(have, num);
 				num = 0;
-			} else {
-				printf("MAXHAVE exceeded\n");
-				fclose(file);
-				return 1;
 			}
 		}
 
 		/* check how many winning numbers we have */
 		qsort(winning, nwinning, sizeof(int), cmpint);
-		num = 0; /* winning numbers we have */
-		while (--nhave >= 0) {
-			if (bsearch(&have[nhave], winning, nwinning, sizeof(int), cmpint)
+		nhavewinning = 0; /* winning numbers we have */
+		while (have != NULL) {
+			have =stpop(have, &num);
+			if (bsearch(&num, winning, nwinning, sizeof(int), cmpint)
 					!= NULL) { /* number we have is in winning set */
-				if (card + ++num >= MAXCARDS) {
+				if (card + ++nhavewinning >= MAXCARDS) {
 					printf("MAXCARDS exceeded\n");
 					fclose(file);
 					return 1;
 				} else {
-					copies[card + num] += copies[card] + 1;
+					copies[card + nhavewinning] += copies[card] + 1;
 				}
 			}
 		}
