d2334be9d07963c4cbbac620b1301b5943f67db1
Sam Anthony <sam@samanthony.xyz>
2023-12-05 20:29:30 -0500

    rename variables

--- 2023/05/p2.c
+++ 2023/05/p2.c
@@ -9,18 +9,17 @@ struct seedrange {
 	unsigned int len;
 };
 
+int readseedranges(struct seedrange seeds[], const char line[]);
 int applymaps(struct seedrange seeds[], int nseeds, const struct map maps[], int nmaps);
-int readseedranges(struct seedrange seedranges[], const char line[]);
-Seed min(const struct seedrange seedranges[], int nseedranges);
+Seed min(const struct seedrange seeds[], int nseeds);
 
-/* bruteforce */
 int
 main()
 {
 	FILE *file;
 	char line[MAXLINE];
-	struct seedrange seedranges[SEEDRANGES];
-	int nseedranges;
+	struct seedrange seeds[SEEDRANGES];
+	int nseeds;
 	int i;
 	struct map maps[MAPS], *mapp;
 
@@ -30,7 +29,7 @@ main()
 	}
 
 	if (fgets(line, MAXLINE, file) == NULL
-			|| (nseedranges = readseedranges(seedranges, line)) < 1) {
+			|| (nseeds = readseedranges(seeds, line)) < 1) {
 		printf("input missing seed list\n");
 		fclose(file);
 		return 1;
@@ -45,19 +44,51 @@ main()
 			}
 			readmap(mapp++, line);
 		} else {
-			nseedranges = applymaps(seedranges, nseedranges, maps, mapp-maps);
-			if (nseedranges < 0) {
+			nseeds = applymaps(seeds, nseeds, maps, mapp-maps);
+			if (nseeds < 0) {
 				fclose(file);
 				return 1;
 			}
 			mapp = maps;
 		}
 	}
-	if ((nseedranges = applymaps(seedranges, nseedranges, maps, mapp-maps)) >= 0)
-		printf("Part 1: %lu\n", min(seedranges, nseedranges));
+	if ((nseeds = applymaps(seeds, nseeds, maps, mapp-maps)) >= 0)
+		printf("Part 2: %lu\n", min(seeds, nseeds));
 
 	fclose(file);
 	return 0;
+}
+
+int
+readseedranges(struct seedrange seeds[], const char line[])
+{
+	int i, n;
+
+	for (i = 0; line[i] != '\0' && line[i] != ':'; i++)
+		;
+	if (line[i] == '\0')
+		return -1;
+
+	n = 0;
+	seeds[n].start = seeds[n].len = 0;
+	while (line[++i] != '\0') {
+		if (isdigit(line[i])) {
+			if (seeds[n].start == 0) {
+				for (; isdigit(line[i]); i++)
+					seeds[n].start = (seeds[n].start * 10) + (line[i] - '0');
+			} else {
+				for (; isdigit(line[i]); i++)
+					seeds[n].len = (seeds[n].len * 10) + (line[i] - '0');
+				if (++n < SEEDRANGES) {
+					seeds[n].start = seeds[n].len = 0;
+				} else {
+					printf("SEEDRANGES exceeded\n");
+					break;
+				}
+			}
+		}
+	}
+	return n;
 }
 
 int
@@ -89,47 +120,15 @@ applymaps(struct seedrange seeds[], int nseeds, const struct map maps[], int nma
 	return nseeds;
 }
 
-int
-readseedranges(struct seedrange seedranges[], const char line[])
-{
-	int i, n;
-
-	for (i = 0; line[i] != '\0' && line[i] != ':'; i++)
-		;
-	if (line[i] == '\0')
-		return -1;
-
-	n = 0;
-	seedranges[n].start = seedranges[n].len = 0;
-	while (line[++i] != '\0') {
-		if (isdigit(line[i])) {
-			if (seedranges[n].start == 0) {
-				for (; isdigit(line[i]); i++)
-					seedranges[n].start = (seedranges[n].start * 10) + (line[i] - '0');
-			} else {
-				for (; isdigit(line[i]); i++)
-					seedranges[n].len = (seedranges[n].len * 10) + (line[i] - '0');
-				if (++n < SEEDRANGES) {
-					seedranges[n].start = seedranges[n].len = 0;
-				} else {
-					printf("SEEDRANGES exceeded\n");
-					break;
-				}
-			}
-		}
-	}
-	return n;
-}
-
 Seed
-min(const struct seedrange seedranges[], int nseedranges)
+min(const struct seedrange seeds[], int nseeds)
 {
 	Seed min;
 	int i;
 
 	min = ~0L;
-	for (i = 0; i < nseedranges; i++)
-		min = (seedranges[i].start < min) ? seedranges[i].start : min;
+	for (i = 0; i < nseeds; i++)
+		min = (seeds[i].start < min) ? seeds[i].start : min;
 	return min;
 }
 
