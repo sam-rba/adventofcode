b7e55db2102e5d87117b73775455304cfda68100
Sam Anthony <sam@samanthony.xyz>
2024-11-10 16:23:01 -0500

    2022/06 gold

---
+++ 2022/06/gold.go
@@ -0,0 +1,6 @@
+package main
+
+const (
+	part      = "gold"
+	markerLen = 14
+)
--- 2022/06/silver.go
+++ 2022/06/silver.go
@@ -1,49 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
-	"os"
-	"slices"
+const (
+	part      = "silver"
+	markerLen = 4
 )
-
-const markerLen = 4
-
-func main() {
-	i, err := findMarker(os.Stdin)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("silver:", i)
-}
-
-func findMarker(stream io.Reader) (int, error) {
-	marker := make([]byte, 0, markerLen)
-	r := bufio.NewReader(stream)
-	for i := 1; ; i++ {
-		b, err := r.ReadByte()
-		if err != nil {
-			return -1, err
-		}
-		marker = append(marker, b)
-		if len(marker) == markerLen {
-			if allDifferent(marker) {
-				fmt.Println(string(marker))
-				return i, nil
-			}
-			marker = marker[1:]
-		}
-	}
-	return -1, fmt.Errorf("no marker")
-}
-
-func allDifferent(s []byte) bool {
-	for i := range s {
-		if slices.Contains(s[i+1:], s[i]) {
-			return false
-		}
-	}
-	return true
-}
